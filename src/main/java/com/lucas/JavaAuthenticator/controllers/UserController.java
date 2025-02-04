c8e1c74fd3f47b9f2356f34dce53418b72942626
Lucas Storck <lucasstorck@icloud.com>
2025-02-04 12:57:49 -0300

    Removed Dockerfile from .gitignore

--- src/main/java/com/lucas/JavaAuthenticator/controllers/UserController.java
+++ src/main/java/com/lucas/JavaAuthenticator/controllers/UserController.java
@@ -10,11 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.annotation.processing.Generated;
@@ -43,6 +41,12 @@ public class UserController {
       throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
+    if (user == null) {
+      user = new Role();
+      user.setName(Role.Values.USER.name());
+      roleRepository.save(user);
+    }
+
     var newUser = new User();
 
     newUser.setUsername(createUserDto.username());
@@ -55,10 +59,55 @@ public class UserController {
     return ResponseEntity.ok().build();
   }
 
-  @GetMapping("/api/users")
+  @GetMapping("/api/user")
   @PreAuthorize("hasAuthority('SCOPE_SUPERUSER')")
-  public ResponseEntity<List<User>> getUsers() {
+  public ResponseEntity<List<User>> listUsers() {
     var users = useRepository.findAll();
     return ResponseEntity.ok(users);
+  }
+
+  @PutMapping("/api/user/{username}")
+  @PreAuthorize("hasAuthority('SCOPE_SUPERUSER') or hasAuthority('SCOPE_USER')")
+  @Transactional
+  public ResponseEntity<Void> updateUser(@PathVariable String username, @RequestBody CreateUserDto createUserDto, JwtAuthenticationToken token) {
+    var user = useRepository.findByUsername(username);
+
+    if (user.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    var updateUser = user.get();
+    updateUser.setUsername(createUserDto.username());
+    updateUser.setEmail(createUserDto.email());
+
+    if (createUserDto.password() != null && !createUserDto.password().isEmpty()) {
+      updateUser.setPassword(bCryptPasswordEncoder.encode(createUserDto.password()));
+    }
+
+    if (!updateUser.getUsername().equals(createUserDto.username())) {
+      var existingUser = useRepository.findByUsername(createUserDto.username());
+      if (existingUser.isPresent()) {
+        throw new ResponseStatusException(HttpStatus.CONFLICT, "THIS USERNAME ALREADY EXIST");
+      }
+    }
+
+    useRepository.save(updateUser);
+    return ResponseEntity.ok().build();
+  }
+
+  @DeleteMapping("/api/user/{username}")
+  @PreAuthorize("hasAuthority('SCOPE_SUPERUSER') or hasAuthority('SCOPE_USER')")
+  @Transactional
+  public ResponseEntity<Void> deleteUser(@PathVariable String username) {
+    var user = useRepository.findByUsername(username);
+    var authorization = useRepository.findByUsername(username);
+
+    if (user.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    useRepository.delete(user.get());
+
+    return ResponseEntity.noContent().build();
   }
 }
